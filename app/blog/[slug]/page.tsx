ed96b419a3b4a83c7a574fcb4664b415fe3d6d55
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2023-07-18 01:05:12 +0530

    initial commit

--- app/blog/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function BlogPost() {
-  return (
-    <div>
-      This page is here to showcase information about a particular blog post.
-    </div>
-  );
-}
--- app/blog/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Blog() {
-  return <div>This page is here to showcase Blogs from me.</div>;
-}
